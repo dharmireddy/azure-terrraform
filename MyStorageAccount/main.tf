5c4b276f51a146a6a74ec754f509e0eec5578b3c
Dharmireddy Krishna <Dharmireddy.Krishna@gds.ey.com>
2022-09-26 12:54:30 +0530

    MystorageAccount

---
+++ MyStorageAccount/main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_resource_group" "Azure-Pipeline" {
+  name     = "Azure-pipelineRG"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "PipelineVnet" {
+  name                = "PipelineVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "West Europe"
+  resource_group_name = azurerm_resource_group.Azure-Pipeline.name
+}
+
+resource "azurerm_subnet" "PipelineSubnet" {
+  name                 = "PipelineSubnet"
+  resource_group_name  = azurerm_resource_group.Azure-Pipeline.name
+  virtual_network_name = azurerm_virtual_network.PipelineVnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
+
+resource "azurerm_storage_account" "PipelineStorageAccount" {
+  name                = "PipelineStorageAccount"
+  resource_group_name = azurerm_resource_group.Azure-Pipeline.name
+
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.PipelineSubnet.id]
+  }
+
+  tags = {
+    environment = "staging"
+  }
+}
